16094364927cd5535c2262cd9b5c721008e86aa3
youngprinnce <ajiboyeadedotun16@gmail.com>
2025-01-23 13:00:20 +0100

    refactor user retrieval methods to use QueryRow and simplify error handling

--- controller/user/store.go
+++ controller/user/store.go
@@ -25,61 +25,44 @@ func (s *Store) CreateUser(user types.User) error {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
-	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
-	if err != nil {
-		return nil, err
-	}
+	row := s.db.QueryRow("SELECT * FROM users WHERE email = ?", email)
 
 	u := new(types.User)
-	for rows.Next() {
-		u, err = scanRowsIntoUser(rows)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if u.ID == 0 {
+	err := row.Scan(
+		&u.ID,
+		&u.FirstName,
+		&u.LastName,
+		&u.Email,
+		&u.Password,
+		&u.CreatedAt,
+	)
+	if err == sql.ErrNoRows {
 		return nil, fmt.Errorf("user not found")
+	} else if err != nil {
+		return nil, err
 	}
 
 	return u, nil
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
-	if err != nil {
-		return nil, err
-	}
+	row := s.db.QueryRow("SELECT * FROM users WHERE id = ?", id)
 
 	u := new(types.User)
-	for rows.Next() {
-		u, err = scanRowsIntoUser(rows)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if u.ID == 0 {
+	err := row.Scan(
+		&u.ID,
+		&u.FirstName,
+		&u.LastName,
+		&u.Email,
+		&u.Password,
+		&u.CreatedAt,
+	)
+	if err == sql.ErrNoRows {
 		return nil, fmt.Errorf("user not found")
+	} else if err != nil {
+		return nil, err
 	}
 
 	return u, nil
 }
 
-func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
-	user := new(types.User)
-
-	err := rows.Scan(
-		&user.ID,
-		&user.FirstName,
-		&user.LastName,
-		&user.Email,
-		&user.Password,
-		&user.CreatedAt,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
-}
