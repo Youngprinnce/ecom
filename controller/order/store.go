56725e686cc34f154bb9bb8bcb2303ffa054c3b0
youngprinnce <ajiboyeadedotun16@gmail.com>
2025-01-23 15:06:24 +0100

    add order store implementation with order and order item types

---
+++ controller/order/store.go
@@ -0,0 +1,36 @@
+package order
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/youngprinnce/go-ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) CreateOrder(order types.Order) (int, error) {
+	ctx := context.Background()
+	result, err := s.db.ExecContext(ctx, "INSERT INTO orders (userId, total, status, address) VALUES (?, ?, ?, ?)", order.UserID, order.Total, order.Status, order.Address)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (s *Store) CreateOrderItem(orderItem types.OrderItem) error {
+	ctx := context.Background()
+	_, err := s.db.ExecContext(ctx, "INSERT INTO order_items (orderId, productId, quantity, price) VALUES (?, ?, ?, ?)", orderItem.OrderID, orderItem.ProductID, orderItem.Quantity, orderItem.Price)
+	return err
+}
--- types/types.go
+++ types/types.go
@@ -58,3 +58,26 @@ type CreateProductPayload struct {
 	Price       float64 `json:"price" validate:"required"`
 	Quantity    int     `json:"quantity" validate:"required"`
 }
+
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"orderID"`
+	ProductID int       `json:"productID"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"createdAt"`
+}
