df1e6035e9e629527bb797fe30fd76e194fdb982
youngprinnce <ajiboyeadedotun16@gmail.com>
2025-01-26 20:44:55 +0100

    refactor database migrations to ensure tables are created only if they do not exist and update primary key definitions

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+// type notification interface {
+// 	importance() int
+// }
+
+// type directMessage struct {
+// 	senderUsername string
+// 	messageContent string
+// 	priorityLevel  int
+// 	isUrgent       bool
+// }
+
+// type groupMessage struct {
+// 	groupName      string
+// 	messageContent string
+// 	priorityLevel  int
+// }
+
+// type systemAlert struct {
+// 	alertCode      string
+// 	messageContent string
+// }
+
+// func (dm directMessage) importance() {
+// 	importanceSc
+// 	if dm.isUrgent {
+
+// 	}
+// }
+
+// func (gm groupMessage) importance() {
+	
+// }
+
+// func (sa systemAlert) importance() {
+	
+// }
+
+// // ?
+
+// func processNotification(n notification) (string, int) {
+// 	// ?
+// }
